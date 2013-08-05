055af22de26e229d03284b9000d24c3b16a8ec5a
liulyliu <liulyliu@sohu-inc.com>
2013-08-05 11:46:18 +0800

    demo

--- demo/conf/mysql.conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('MYSQL_NAME','fortest');
-define('MYSQL_USER','root');
-define('MYSQL_PASS','123456');
-define('MYSQL_HOST','localhost');
-global $db;
-$db = new MySQL(MYSQL_NAME, MYSQL_USER, MYSQL_PASS,MYSQL_HOST);
-?>
